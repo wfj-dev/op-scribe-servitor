20ae91024a9ac329c0e105e8d4145fd9518be849
Forge Master Jules <312649628+watchfortressjericho-svg@users.noreply.github.com>
2026-06-08 02:14:42 -0400

    black listed strats

--- opscribe/target_packages_ops.py
+++ opscribe/target_packages_ops.py
@@ -743,7 +743,14 @@ def _draw_strats(rep: float, active_strats: list, mode: str = "Hard-Strat") -> d
         "Broken Bulwark",
         "Fallen Vanguard",
         "Heavy Burden",
-        "Tactical Weakness"
+        "Tactical Weakness",
+        "Booby Trap",
+        "Depleted Armour",
+        "Empathy",
+        "Equipment Malfunction",
+        "Fatal Contamination",
+        "Hazardous Environment",
+        "Shadow of the Warp"
     } | omega_excluded
 
     buffs = [s for s in active_strats if s["type"] == "buff" and s["name"] not in mode_excluded and s["name"] != "Intelligence Lapse"]
